7ef1820ed5043d54d1a1f95f25b789de0f4dd81d
Soumya <soumyachopra@gmail.com>
2018-10-09 08:33:47 +0530

    practice

---
+++ Codeacademy/Syntax/Twotypesofdivision.py
@@ -0,0 +1,6 @@
+cucumbers=100
+num_people=6
+whole_cucumbers_per_person=cucumbers/num_people
+print whole_cucumbers_per_person
+float cucumbers_per_person=cucumbers./6.
+print float_cucumbers_per_person
